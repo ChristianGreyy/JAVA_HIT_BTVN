ebe7baeed041d024ecff15b5427257170bfde572
Christian Grey <truongthanhhung2k2@gmail.com>
2021-10-12 23:24:28 +0700

    BTVN buoi 1

---
+++ BTVN_buoi1/src/Bai2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bai2 {
+
+    public static void main(String[] args) {
+
+                System.out.println("*      *     *     * * * *    * * * *    *      *");
+                System.out.println("|      |    / \\    |       *  |       *   \\   /");
+                System.out.println("* ---- *   *---*   * * * *    | * * *       *");
+                System.out.println("|      |  /    \\   |          |             |");
+                System.out.println("*      * *       * *          *             |");
+                System.out.print('\n');
+                System.out.println("*      *  ******  *         *        *");
+                System.out.println("|  \\   |  |        \\      /   \\     /");
+                System.out.println("*   *  *  ******     *   *     *   *");
+                System.out.println("|    \\ |  |           \\ /       \\ /  ");
+                System.out.println("*      *  ******       *         * ");
+                System.out.print("\n");
+                System.out.println("*   *  ******      *      * * * * ");
+                System.out.println(" \\ /   |          / \\     |       *");
+                System.out.println("  *    ******    *---*    * * * * ");
+                System.out.println("  |    |        /     \\   |      \\ ");
+                System.out.println("  *    ******  *       *  *       *");
+    }
+
+
+}
